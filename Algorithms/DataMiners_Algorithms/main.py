6aba579fc56d0603a8cb78b5d97d277dcf4e3b7c
Arnout112 <arnout.r.t.smit@gmail.com>
2024-01-26 13:25:10 +0100

    Added transformation method for testing purposes.

--- Algorithms/DataMiners_Algorithms/main.py
+++ Algorithms/DataMiners_Algorithms/main.py
@@ -2,6 +2,7 @@
 # Authors: Arnout Smit, Luca Doenen
 from typing import Tuple, Iterable
 from math import sqrt
+from collections import defaultdict
 
 def euclidian_distance(point1: Iterable[float], point2: Iterable[float]) -> float:
     """Calculate the euclidian distance between two points in space.
@@ -73,28 +74,6 @@ if __name__ == '__main__':
     print("Predeccesors:", dijkstra_algorithm(graaf1, 3, 6)[2])
     print("Afstanden:", dijkstra_algorithm(graaf1, 3, 6)[0])
 
-    plattegrond = {
-        (18.75, 31),
-        (19.25, 31),
-        (19.25, 30.5),
-        (19.25, 29.5),
-        (19.25, 28.5),
-        (19.25, 28),
-        (19.25, 27.5),
-        (19.25, 27),
-        (19.25, 26.5),
-        (19.25, 26),
-        (19.25, 25.5),
-        (19.25, 25),
-        (19.25, 24.5),
-        (19.25, 24),
-        (19.25, 23.5),
-        (19.25, 23),
-        (19.25, 22.5),
-        (19.25, 22),
-        (19.25, 21.5)
-    }
-
 # node : coordinates
     nodes_coords = {
         "B3-1":  (0.75, 1)   ,
@@ -135,25 +114,105 @@ if __name__ == '__main__':
         "B3-15" : [["B.3.104"]                                  , ["B3-16"]         , (7.75, 3)   ],
         "B3-16" : [["B3.n2"]                                    , []                , (8, 1)      ],
         "B3-17" : [["trap C.3.1"]                               , []                , (9, 5)      ],   #buren?
-    } # Hans zegt over nooduitgang-functie: vraag korste route naar een (nood)trap gebruik, noodtrap als eindnode.
+    }
+    # {lokaal : [node]}
+    lokalen = {x: key for key, value in graph_B3.items() for x in value[0]}
+    print(lokalen)
 
-    # x = {key: {value[1]: value[0] for _ in range(len(value[1]))} for key, value in graph_B3.items()}
-    # print(x)
-    #graph_B3[value[1][0]][2]
+    l = {"B3-1"  : ["B.3.309", "B.3.311"]                       ,
+"B3-2"  : ["NOOD3-1" , "B.3.310"]                      ,
+"B3-3"  : ["B.3.305", "B.3.308"]                       ,
+"B3-4"  : ["B.3.306", "B.3.304"]                       ,
+"B3-5"  : ["B.3.302"           ]                       ,
+"B3-6"  : ["B.3.305", "B.3.300"]                       ,
+"B3-7"  : ["B.3.210"           ]                       ,
+"B3-8"  : ["trap B3"        ]                          ,
+"B3-9"  : ["B.3.210", "B.3.221"]                       ,
+"B3-10" : ["B.3.217"           ]                       ,
+"B3-11" : ["B.3.210", "B.3.208", "B.3.217", "B.3.215" ],
+"B3-12" : ["B.3.213" ]                                 ,
+"B3-13" : ["B.3.209", "B.3.211", "B.3.206", "B.3.200"] ,
+"B3-14" : ["B.3.105", "B.3.103", "B.3.200", "B.3.107"] ,
+"B3-15" : ["B.3.104"]                                  ,
+"B3-16" : ["NOOD3-2"]                                  ,
+"B3-17" : ["trap C3"]                                  ,
 
-    # Python code to demonstrate dictionary
-    # comprehension
+"B2-1"  : ["B.2.309", "B.2.311"]                       ,
+"B2-2"  : ["NOOD2-1", "B.2.310"]                       ,
+"B2-3"  : ["B.2.305", "B.2.308"]                       ,
+"B2-4"  : ["B.2.306", "B.2.304"]                       ,
+"B2-5"  : ["B.2.302"]                                  ,
+"B2-6"  : ["B.2.305", "B.2.300"]                       ,
+"B2-7"  : ["B.2.210"]                                  ,
+"B2-8"  : ["trap B2"]                                  ,
+"B2-9"  : ["B.2.210", "B.2.221"]                       ,
+"B2-10" : ["B.2.217"]                                  ,
+"B2-11" : ["B.2.210", "B.2.208", "B.2.217", "B.2.215"] ,
+"B2-12" : ["B.2.213"]                                  ,
+"B2-13" : ["B.2.209", "B.2.211", "B.2.206", "B.2.200"] ,
+"B2-14" : ["B.2.105", "B.2.103", "B.2.200", "B.2.107"] ,
+"B2-15" : ["B.2.104"]                                  ,
+"B2-16" : ["NOOD2-2"]                                  ,
+"B2-17" : ["trap C2"]                                  ,
 
-    # Lists to represent keys and values
-    keys = ['a', 'b', 'c', 'd', 'e']
-    values = [1, 2, 3, 4, 5]
+"B1-1"  : ["B.1.309", "B.1.311"]                       ,
+"B1-2"  : ["NOOD1-1", "B.1.310"]                       ,
+"B1-3"  : ["B.1.305", "B.1.308"]                       ,
+"B1-4"  : ["B.1.306", "B.1.304"]                       ,
+"B1-5"  : ["B.1.302"]                                  ,
+"B1-6"  : ["B.1.305", "B.1.300"]                       ,
+"B1-7"  : ["B.1.210"]                                  ,
+"B1-8"  : ["trap B1"]                                  ,
+"B1-9"  : ["B.1.210", "B.1.221"]                       ,
+"B1-10" : ["B.1.217"]                                  ,
+"B1-11" : ["B.1.210", "B.1.208", "B.1.217", "B.1.215"] ,
+"B1-12" : ["B.1.213"]                                  ,
+"B1-13" : ["B.1.209", "B.1.211", "B.1.206", "B.1.200"] ,
+"B1-14" : ["B.1.105", "B.1.103", "B.1.200", "B.1.107"] ,
+"B1-15" : ["B.1.104"]                                  ,
+"B1-16" : ["NOOD1-2"]                                  ,
+"B1-17" : ["trap C1"]                                  ,
 
-    # but this line shows dict comprehension here
-    myDict = {k: v for (k, v) in zip(keys, values)}
+"B0-1"  : ["B.0.309", "B.0.311"]                       ,
+"B0-2"  : ["NOOD0-1", "B.0.310"]                       ,
+"B0-3"  : ["B.0.305", "B.0.308"]                       ,
+"B0-4"  : ["B.0.306", "B.0.304"]                       ,
+"B0-5"  : ["B.0.302"]                                  ,
+"B0-6"  : ["B.0.305", "B.0.300"]                       ,
+"B0-7"  : ["B.0.210"]                                  ,
+"B0-8"  : ["trap B0"]                                  ,
+"B0-9"  : ["B.0.210", "B.0.221"]                       ,
+"B0-10" : ["B.0.217"]                                  ,
+"B0-11" : ["B.0.210", "B.0.208", "B.0.217", "B.0.215"] ,
+"B0-12" : ["B.0.213"]                                  ,
+"B0-13" : ["B.0.209", "B.0.211", "B.0.206", "B.0.200"] ,
+"B0-14" : ["B.0.105", "B.0.103", "B.0.200", "B.0.107"] ,
+"B0-15" : ["B.0.104"]                                  ,
+"B0-16" : ["NOOD0-2"]                                  ,
+"B0-17" : ["trap C0"]                                  ,
+"B0-18" : ["Hoofdingang"]                              ,
+"B0-19" : ["Cafetaria"]                                ,}
 
-    # We can use below too
-    # myDict = dict(zip(keys, values))
+    lokalen = {x: [key] for key, value in l.items() for x in value}
+    print(lokalen)
 
-    print(myDict)
+    swapped_dict = defaultdict(list)
 
+    for key, value in l.items():
+        for v in value:
+            swapped_dict[v].append(key)
+
+    print(swapped_dict)
+    swapped_lokalen_dict = {'B.3.309': ['B3-1'], 'B.3.311': ['B3-1'], 'NOOD3-1': ['B3-2'], 'B.3.310': ['B3-2'], 'B.3.305': ['B3-3', 'B3-6'], 'B.3.308': ['B3-3'], 'B.3.306': ['B3-4'], 'B.3.304': ['B3-4'], 'B.3.302': ['B3-5'], 'B.3.300': ['B3-6'], 'B.3.210': ['B3-7', 'B3-9', 'B3-11'], 'trap B3': ['B3-8'], 'B.3.221': ['B3-9'], 'B.3.217': ['B3-10', 'B3-11'], 'B.3.208': ['B3-11'], 'B.3.215': ['B3-11'], 'B.3.213': ['B3-12'], 'B.3.209': ['B3-13'], 'B.3.211': ['B3-13'], 'B.3.206': ['B3-13'], 'B.3.200': ['B3-13', 'B3-14'], 'B.3.105': ['B3-14'], 'B.3.103': ['B3-14'], 'B.3.107': ['B3-14'], 'B.3.104': ['B3-15'], 'NOOD3-2': ['B3-16'], 'trap C3': ['B3-17'], 'B.2.309': ['B2-1'], 'B.2.311': ['B2-1'], 'NOOD2-1': ['B2-2'], 'B.2.310': ['B2-2'], 'B.2.305': ['B2-3', 'B2-6'], 'B.2.308': ['B2-3'], 'B.2.306': ['B2-4'], 'B.2.304': ['B2-4'], 'B.2.302': ['B2-5'], 'B.2.300': ['B2-6'], 'B.2.210': ['B2-7', 'B2-9', 'B2-11'], 'trap B2': ['B2-8'], 'B.2.221': ['B2-9'], 'B.2.217': ['B2-10', 'B2-11'], 'B.2.208': ['B2-11'], 'B.2.215': ['B2-11'], 'B.2.213': ['B2-12'], 'B.2.209': ['B2-13'], 'B.2.211': ['B2-13'], 'B.2.206': ['B2-13'], 'B.2.200': ['B2-13', 'B2-14'], 'B.2.105': ['B2-14'], 'B.2.103': ['B2-14'], 'B.2.107': ['B2-14'], 'B.2.104': ['B2-15'], 'NOOD2-2': ['B2-16'], 'trap C2': ['B2-17'], 'B.1.309': ['B1-1'], 'B.1.311': ['B1-1'], 'NOOD1-1': ['B1-2'], 'B.1.310': ['B1-2'], 'B.1.305': ['B1-3', 'B1-6'], 'B.1.308': ['B1-3'], 'B.1.306': ['B1-4'], 'B.1.304': ['B1-4'], 'B.1.302': ['B1-5'], 'B.1.300': ['B1-6'], 'B.1.210': ['B1-7', 'B1-9', 'B1-11'], 'trap B1': ['B1-8'], 'B.1.221': ['B1-9'], 'B.1.217': ['B1-10', 'B1-11'], 'B.1.208': ['B1-11'], 'B.1.215': ['B1-11'], 'B.1.213': ['B1-12'], 'B.1.209': ['B1-13'], 'B.1.211': ['B1-13'], 'B.1.206': ['B1-13'], 'B.1.200': ['B1-13', 'B1-14'], 'B.1.105': ['B1-14'], 'B.1.103': ['B1-14'], 'B.1.107': ['B1-14'], 'B.1.104': ['B1-15'], 'NOOD1-2': ['B1-16'], 'trap C1': ['B1-17'], 'B.0.309': ['B0-1'], 'B.0.311': ['B0-1'], 'NOOD0-1': ['B0-2'], 'B.0.310': ['B0-2'], 'B.0.305': ['B0-3', 'B0-6'], 'B.0.308': ['B0-3'], 'B.0.306': ['B0-4'], 'B.0.304': ['B0-4'], 'B.0.302': ['B0-5'], 'B.0.300': ['B0-6'], 'B.0.210': ['B0-7', 'B0-9', 'B0-11'], 'trap B0': ['B0-8'], 'B.0.221': ['B0-9'], 'B.0.217': ['B0-10', 'B0-11'], 'B.0.208': ['B0-11'], 'B.0.215': ['B0-11'], 'B.0.213': ['B0-12'], 'B.0.209': ['B0-13'], 'B.0.211': ['B0-13'], 'B.0.206': ['B0-13'], 'B.0.200': ['B0-13', 'B0-14'], 'B.0.105': ['B0-14'], 'B.0.103': ['B0-14'], 'B.0.107': ['B0-14'], 'B.0.104': ['B0-15'], 'NOOD0-2': ['B0-16'], 'trap C0': ['B0-17'], 'Hoofdingang': ['B0-18'], 'Cafetaria': ['B0-19']}
+    print(swapped_lokalen_dict["B.3.305"])
+
+
+    test_graph_B3 = {key: {x: euclidian_distance(value[2], graph_B3[x][2]) for x in value[1]} for key, value in graph_B3.items()}
+    print(test_graph_B3)
+    print("Korste weg:", dijkstra_algorithm(test_graph_B3, "B3-1", "B3-17")[3])
+    # print("Volgorde van bezochte knopen:", dijkstra_algorithm(test_graph_B3, "B3-1", "B3-17")[1])
+    # print("Predeccesors:", dijkstra_algorithm(test_graph_B3, "B3-1", "B3-17")[2])
+    print("Afstanden:", dijkstra_algorithm(test_graph_B3, "B3-1", "B3-17")[0])
+
+    # Hans zegt over nooduitgang-functie: vraag korste route naar een (nood)trap gebruik, noodtrap als eindnode.
     # kleine lokaaltjes en lokalen die aan elkaar verbonden zijn hebben we genegeerd. Het is unlikely dat een klein lokaaltje wordt gebruikt voor een presentatie en een route die dwars door een ander lokaal gaat is in de meeste gevallen ongewest.
